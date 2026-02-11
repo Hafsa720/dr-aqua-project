c890ec7d62d44aefb9e1b65028e8a516538dd2aa
m-hamidmehmood <saim008865@gmail.com>
2026-02-11 10:44:10 +0500

    Update Navigation - remove dashboard references

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,11 +1,6 @@
 'use client';
 
-import {
-  Languages,
-  Menu,
-  ShoppingCart,
-  X,
-} from 'lucide-react';
+import { Languages, Menu, ShoppingCart, X } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
